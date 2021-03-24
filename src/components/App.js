18a2f7badbd5cc980f7defb0fba74b72fe19bc68
unknown <ehab.al.mardoud@gmail.com>
2021-03-24 16:12:25 +0100

    Added React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
+import { Switch, Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
-    render(){
+    render() {
         return <div>
             <h1>Welcome to my boilerplate!</h1>
+            <Switch>
+                <Route path='/firstRoute'>
+                    <h2>This is the first route! React Router is working!</h2>
+                    <NavLink to='/' activeClassName="hurray">
+                        <h2>Back to home</h2>
+                    </NavLink>
+                </Route>
+                <Route path='/'>
+                    <h2>This is the home Page</h2>
+                    <NavLink to='/firstRoute' activeClassName="hurray">
+                        <h2>Go to the first route</h2>
+                    </NavLink>
+                </Route>
+            </Switch>
         </div>
     }
 }
